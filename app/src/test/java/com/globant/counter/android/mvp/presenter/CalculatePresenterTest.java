adc5c0c8be37de89176665e5cc9b57dc30e197ad
s.ruiz <s.ruiz@globant.com>
2017-02-21 15:13:26 -0300

    Unit Test for CalculatePresenter

--- app/src/test/java/com/globant/counter/android/mvp/presenter/CalculatePresenterTest.java
+++ app/src/test/java/com/globant/counter/android/mvp/presenter/CalculatePresenterTest.java
@@ -1,24 +1,41 @@
 package com.globant.counter.android.mvp.presenter;
 
+import com.globant.counter.android.mvp.model.CalculateModel;
+import com.globant.counter.android.mvp.view.CalculateView;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.mock;
 
 /**
  * @author s.ruiz
  */
 public class CalculatePresenterTest {
-    @Test
-    public void onEqualPressed() throws Exception {
 
+    private CalculateView view;
+    private CalculatePresenter presenter;
+    private CalculateModel model;
+
+    @Before
+    public void setup() {
+        model = new CalculateModel();
+        view = mock(CalculateView.class);
+        presenter = new CalculatePresenter(view, model);
     }
 
     @Test
-    public void onInput() throws Exception {
-
+    public void operationValid() throws Exception {
+        Mockito.when(view.getValues()).thenReturn("2+3=");
+        presenter.onOperation(new CalculateView.OnOperationEvent());
     }
 
-    @Test
-    public void isValid() throws Exception {
-
+    /*NullPointerException expected because the view is mocked
+    * and the method view.getContext() return Null*/
+    @Test(expected = NullPointerException.class)
+    public void operationInvalid() throws Exception {
+        Mockito.when(view.getValues()).thenReturn("2/0=");
+        presenter.onOperation(new CalculateView.OnOperationEvent());
     }
-
 }
